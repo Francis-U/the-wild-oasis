69d0ba04735dbf78857cda86d0e6d498d960891a
Francis <engukatu@yahoo.com>
2026-05-07 05:35:42 +0100

    Initial commit

---
+++ src/pages/Cabins.jsx
@@ -0,0 +1,36 @@
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+
+function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
+  // useEffect(function () {
+  //   getCabins().then((data) => console.log(data));
+  // }, []);
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+        {/* <img
+        src="https://nbcsppxfqeuadulszrvi.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+        alt=""
+      /> */}
+      </Row>
+
+      <Row>
+        <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
+      </Row>
+    </>
+  );
+}
+
+export default Cabins;
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://nbcsppxfqeuadulszrvi.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5iY3NwcHhmcWV1YWR1bHN6cnZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNzI2ODEsImV4cCI6MjA5Mjk0ODY4MX0.RSRSI8ESgBiJGjIa3vmqc-rRaAU3Oja7Bh9N6SarfSM";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
